4887541f66ff5241830689dd19dea2b1d9447633
Afei <1337853971@qq.com>
2018-05-27 10:20:54 +0800

    php version limited

--- index.php
+++ index.php
@@ -10,6 +10,11 @@ define("APP",__DIR__."/App/");
 define("COMMONPATH",__DIR__."/Common/");
 define("LIBPATH",__DIR__."/Common/lib/");
 define("EXT",".php");
+//版本检测
+if(!version_compare(PHP_VERSION,"7.0","ge")){
+    echo "当前版本PHP：".PHP_VERSION." 低于7.0";exit;
+};
+
 include ROOTPATH."/vendor/autoload.php";
 include ROOTPATH."/Common/Loader.php";
 spl_autoload_register("\\Common\\Loader::_autoload");
